65b8c750595832858b9b71fb4c30f37aa2ec2853
Aditya <adimail2404@gmail.com>
2023-09-01 12:32:06 +0530

    updates asmt 04

--- C++/semester_3/oop4.cpp
+++ C++/semester_3/oop4.cpp
@@ -2,43 +2,54 @@
 #include <vector>
 #include <string>
 
-using namespace std;;
+using namespace std;
 
-class startup{
-    public:
-        string name = "book my sport";
-        vector<string> founders={"Aditya Godse", "Atharva Nawale", "Aditya Pawar"};
-    private:
-        vector<string> techstack={"MongoDB","FastAPI","ElasticSearch"};
-        int fundings = 0;
+class startup {
+public:
+    string name;
+    vector<string> founders;
     
+private:
+    vector<string> techstack;
+    int fundings;
+    
+public:
+    startup(string startupName, vector<string> startupFounders, vector<string> startupTechstack, int startupFundings) {
+        name = startupName;
+        founders = startupFounders;
+        techstack = startupTechstack;
+        fundings = startupFundings;
+    }
     friend class investor;
 };
 
-class investor{
-    public:
-        void display(startup& obj){
-            
-            cout<<"Private variables: \n";
-            cout<<"Name: "<<obj.name<<endl;
-            cout<<"Founders: \n";
-            for(int i=0;i<obj.founders.size();i++){
-                cout<<"-"<<obj.founders[i]<<endl;
-            };
-
-            cout<<endl;
-
-            cout<<"Protected variables: \n";
-            cout<<"Fundings: "<<obj.fundings<<endl;
-            cout<<"Tech Stack: \n";
-            for(int i=0;i<obj.techstack.size();i++){
-                cout<<"-"<<obj.techstack[i]<<endl;
-            };
+class investor {
+public:
+    void display(startup& obj) {
+        cout << "Private variables: \n";
+        cout << "Name: " << obj.name << endl;
+        cout << "Founders: \n";
+        for (int i = 0; i < obj.founders.size(); i++) {
+            cout << "-" << obj.founders[i] << endl;
         };
+
+        cout << endl;
+
+        cout << "Protected variables: \n";
+        cout << "Fundings: " << obj.fundings << endl;
+        cout << "Tech Stack: \n";
+        for (int i = 0; i < obj.techstack.size(); i++) {
+            cout << "-" << obj.techstack[i] << endl;
+        };
+    }
 };
 
-int main(){
-    startup bookmysport;
+int main() {
+    vector<string> founders = { "Aditya Godse", "Atharva Nawale", "Aditya Pawar" };
+    vector<string> techstack = { "MongoDB", "FastAPI", "ElasticSearch" };
+    int fundings = 0;
+    startup bookmysport("Book My Sport", founders, techstack, fundings);
+
     investor angel;
     angel.display(bookmysport);
 
